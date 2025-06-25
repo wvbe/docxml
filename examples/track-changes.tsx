09e7f6056aecf9a46105b42c98f552f792efa0dd
jogawebb <webbjg@gmail.com>
2025-06-25 13:40:34 +0200

    Add example file for track changes features (#26)

---
+++ examples/track-changes.tsx
@@ -0,0 +1,61 @@
+/** @jsx  Docx.jsx */
+import Docx, {
+	Paragraph,
+	Section,
+	Text,
+	TextAddition,
+	TextDeletion,
+} from '../mod.ts';
+
+// Create a new .docx file with track changes enabled.
+const docxFile = Docx.fromNothing().withSettings({
+	isTrackChangesEnabled: true,
+});
+
+// Create a new paragraph that includes text, a text deletion, and a text addition.
+const testParagraph = new Paragraph(
+	{},
+	new Text({}, 'Hello, '),
+	new TextDeletion(
+		{
+			id: 2,
+			author: 'Gabe',
+			date: new Date(),
+		},
+		new Text({}, 'nighttime')
+	),
+	new TextAddition(
+		{ id: 2, author: 'Paul Simon', date: new Date() },
+		new Text({}, 'darkness')
+	),
+	new Text({}, ' my old friend.'),
+	new TextAddition(
+		{ id: 1, author: 'Gabe', date: new Date() },
+		new Text({}, ` I've come to talk with you again.`)
+	)
+);
+
+// Create a section as the parent of our new paragraph.
+const testSection = new Section({}, testParagraph);
+
+// Set that section as the content of our document.
+docxFile.document.set(testSection);
+
+// Save our document.
+await docxFile.toFile('track-changes.docx');
+
+// Alternatively, you can use JSX:
+await Docx.fromJsx(
+	<Section>
+		<Paragraph>
+			<Text>Four score and </Text>
+			<TextDeletion id={1} author="Gabe" date={new Date()}>
+				<Text>six</Text>
+			</TextDeletion>
+			<TextAddition id={1} author="Gabe" date={new Date()}>
+				<Text>seven</Text>
+			</TextAddition>
+			<Text> years ago...</Text>
+		</Paragraph>
+	</Section>
+).toFile('track-changes-jsx.docx');
